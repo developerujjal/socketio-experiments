1753668c95bdc42c80d16127cd305ca6026e7271
developerujjal <ujjal.vision02@gmail.com>
2025-03-29 20:43:48 +0600

    understand about emit events and brodcasting and also nameSpace

--- index.js
+++ index.js
@@ -15,37 +15,60 @@ app.get('/', (req, res) => {
 })
 
 
-io.on('connection', (socket) => {
-    console.log("A user connected; ", socket.id)
+// io.on('connection', (socket) => {
+//     console.log("A user connected; ", socket.id)
 
-    // setTimeout(()=> {
-    //     socket.send("This is Barry Allen")
-    // }, 5000)
-
-
-    // setInterval(() => {
-    //     const time = new Date().getTime();
-    //     socket.send(time)
-    // }, 100)
-
-    // setTimeout(() => {
-    //     socket.emit("SendData", "We are the Best")
-    // }, 2000)
-
-    
-    // socket.on("message", (value) => {
-    //     console.log(value)
-    // })
+//     // setTimeout(()=> {
+//     //     socket.send("This is Barry Allen")
+//     // }, 5000)
 
 
-    socket.on("textValue", (value) => {
-        console.log(value)
-    })
+//     // setInterval(() => {
+//     //     const time = new Date().getTime();
+//     //     socket.send(time)
+//     // }, 100)
+
+//     // setTimeout(() => {
+//     //     socket.emit("SendData", "We are the Best")
+//     // }, 2000)
 
 
-    socket.on('disconnect', () => {
-        console.log("User disconnected")
-    })
+//     // socket.on("message", (value) => {
+//     //     console.log(value)
+//     // })
+
+
+//     // socket.on("textValue", (value) => {
+//     //     console.log(value)
+//     // })
+
+//     //===============> Broadcasting Event <=============
+
+//     //Older syntax but still work
+//     // io.sockets.emit('brodcas', "Helle Everyone")
+
+
+//     //Morder syntax
+//     // io.emit("newBrodcast", "Hello from server side")
+
+//     socket.on('disconnect', () => {
+//         console.log("User disconnected")
+//     })
+// })
+
+
+
+//Custom Namespaces
+const guestNamespace = io.of('/guest');
+const adminNamespace = io.of('/admin');
+
+guestNamespace.on('connection', (socket) => {
+    guestNamespace.emit("mybrodcast", "Hello from Guest")
+})
+
+
+adminNamespace.on('connection', (socket) => {
+    adminNamespace.emit('adminbrodcast', 'Hello from Admin')
 })
 
 
